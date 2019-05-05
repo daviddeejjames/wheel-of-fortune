ccff2a69f24ab3735bb951771c29a6587397d586
daviddeejjames <davidj28827@gmail.com>
2019-05-05 19:37:52 +1000

    Updates

---
+++ precache-manifest.7a026d896d3e840fe8a9484dc15e96f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627a73347d67a2740e0e313fc1a0a771",
+    "url": "/daviddeejjames/wheel-of-fortune/index.html"
+  },
+  {
+    "revision": "135a9ce4ae0942ed843f",
+    "url": "/daviddeejjames/wheel-of-fortune/static/js/2.c4a177cf.chunk.js"
+  },
+  {
+    "revision": "4e9dea1a530e286c4f8e",
+    "url": "/daviddeejjames/wheel-of-fortune/static/js/main.d4eebe3d.chunk.js"
+  },
+  {
+    "revision": "b0124a70f83ee4b0e579",
+    "url": "/daviddeejjames/wheel-of-fortune/static/js/runtime~main.c0aa87c1.js"
+  }
+]);
